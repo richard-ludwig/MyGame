e4daf0c1ede9313c8050820e4469d60e72febf18
Richard Ludwig <richard.ludwig2001@outlook.com>
2023-06-05 10:02:22 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Classe de execução.
+ */
+public class Main {
+    /**
+     * Método de execução(main)
+     * @param args argumentos recebidos via console;
+     */
+    public static void main(String[] args) {
+
+    }
+}
